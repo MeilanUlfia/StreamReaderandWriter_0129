649d2db57e344a3700ea9cd3a19be62f3542d220
meilan <meilanulfia@gmail.com>
2025-06-10 19:12:52 +0700

    memanggil array elemen ke 5

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -13,12 +13,14 @@ int main()
     {
        array<int, 3> data = { 10, 20, 30 };
        // pesan array integer 3 elemen
-      
+       cout <<data.at(5)<<endl;
+       // memanggil array elemen ke 5
     }
     catch (exception& e)
     {
-        
+       
     }
 
-   
+    
+    
 }
