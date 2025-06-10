326176007d68a6aa7acdec1113aaf2c9fc3efb60
meilan <meilanulfia@gmail.com>
2025-06-10 19:12:07 +0700

    add main pesan array integer 3 elemen

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,21 @@
 // untuk obyek exception yang akan digunakan
 #include <array>
 // untuk array exception yang akan digunakan
+using namespace std;
+
+int main() 
+{
+    cout << "Awal Program" << endl; // penanda 1:....
+    try
+    {
+       array<int, 3> data = { 10, 20, 30 };
+       // pesan array integer 3 elemen
+      
+    }
+    catch (exception& e)
+    {
+        
+    }
+
+   
+}
