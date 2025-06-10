02175392ec8fda61a4ee79c4eb4d777996ebd566
meilan <meilanulfia@gmail.com>
2025-06-10 18:02:25 +0700

    menutup file keluaran dan membuka file serta menunjuk untuk dibaca

--- ioFile.cpp
+++ ioFile.cpp
@@ -20,5 +20,12 @@ int main() {
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl; 
     }
+    // selesai dalam menulis sekarang tutup file nya
+    outfile.close();
+    // membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+    cout << endl << ">= Membuka dan membaca file " << endl;
    
 }
